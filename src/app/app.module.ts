3e48ee503b22181ec7360e24840f75d56e0aeafd
pasqualedazzeo <48350190+pasqualedazzeo@users.noreply.github.com>
2023-07-13 16:14:06 +0200

    portfolio first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
